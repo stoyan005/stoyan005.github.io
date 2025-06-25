c89d7bc1d243709740f142f48ea3492c1538d7d2
Stoyan Ivanov <ivanov.stoyan100@gmail.com>
2025-06-25 20:28:08 +0100

    updated the badges and js of projects

--- home.js
+++ home.js
@@ -1,8 +1,4 @@
-const words = [
-    "HTML & CSS",
-    "JavaScript",
-    "Python"
-];
+const words = ["HTML & CSS", "JavaScript", "Python"];
 
 let wordIndex = 0;
 let charIndex = 0;
@@ -10,24 +6,22 @@ let isDeleting = false;
 const el = document.querySelector(".languages");
 
 function type() {
-const currentWord = words[wordIndex];
-el.textContent = currentWord.slice(0, charIndex);
+	const currentWord = words[wordIndex];
+	el.textContent = currentWord.slice(0, charIndex);
 
-if (!isDeleting && charIndex < currentWord.length) {
-    charIndex++;
-    setTimeout(type, 100);
-
-} else if (isDeleting && charIndex > 0) {
-    charIndex--;
-    setTimeout(type, 50);
-    
-} else {
-    isDeleting = !isDeleting;
-    if (!isDeleting) {
-    wordIndex = (wordIndex + 1) % words.length;
-    }
-    setTimeout(type, 1000);
-}
+	if (!isDeleting && charIndex < currentWord.length) {
+		charIndex++;
+		setTimeout(type, 100);
+	} else if (isDeleting && charIndex > 0) {
+		charIndex--;
+		setTimeout(type, 50);
+	} else {
+		isDeleting = !isDeleting;
+		if (!isDeleting) {
+			wordIndex = (wordIndex + 1) % words.length;
+		}
+		setTimeout(type, 1000);
+	}
 }
 
 window.addEventListener("DOMContentLoaded", type);
--- projects.js
+++ projects.js
@@ -1,35 +1,39 @@
-const counterEl = document.querySelector('.counter_1');
-let counterNumber = 0;
+function main() {
+	const counterEl = document.querySelector(".counter_1");
+	let counterNumber = 0;
 
-const updateCounter = setInterval(function () {
-    counterNumber++;
-    counterEl.textContent = counterNumber;
+	const updateCounter = setInterval(function () {
+		counterNumber++;
+		counterEl.textContent = counterNumber;
 
-    if (counterNumber >= 10) {
-        clearInterval(updateCounter);
-    }
-},300)
+		if (counterNumber >= 10) {
+			clearInterval(updateCounter);
+		}
+	}, 300);
 
-const counterEle = document.querySelector('.counter_2');
-let counterNumber1 = 0;
+	const counterEle = document.querySelector(".counter_2");
+	let counterNumber1 = 0;
 
-const updateCounter1 = setInterval(function () {
-    counterNumber1++;
-    counterEle.textContent = counterNumber1;
+	const updateCounter1 = setInterval(function () {
+		counterNumber1++;
+		counterEle.textContent = counterNumber1;
 
-    if (counterNumber1 >= 11) {
-        clearInterval(updateCounter1);
-    }
-},260)
+		if (counterNumber1 >= 11) {
+			clearInterval(updateCounter1);
+		}
+	}, 260);
 
-const counterElem = document.querySelector('.counter_3');
-let counterNumber2 = 0;
+	const counterElem = document.querySelector(".counter_3");
+	let counterNumber2 = 0;
 
-const updateCounter2 = setInterval(function () {
-    counterNumber2++;
-    counterElem.textContent = counterNumber2;
+	const updateCounter2 = setInterval(function () {
+		counterNumber2++;
+		counterElem.textContent = counterNumber2;
 
-    if (counterNumber2 >= 150) {
-        clearInterval(updateCounter2);
-    }
-},20)
+		if (counterNumber2 >= 150) {
+			clearInterval(updateCounter2);
+		}
+	}, 20);
+}
+
+setTimeout(main, 2200);
